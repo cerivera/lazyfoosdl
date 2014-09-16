610e47377c335c597ada36be719f8a81f9a8a41f
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2014-09-16 05:58:02 -0500

    Lesson 04

--- LazyFooTut/main.cpp
+++ LazyFooTut/main.cpp
@@ -1,18 +1,92 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <string>
+
+// Key press surfaces constants
+enum KeyPressSurfaces {
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+// Starts up SDL and creates window
+bool init();
+
+// Loads an image into memory
+bool loadMedia();
+
+// Frees up memory and quits SDL
+void close();
+
+// Loads a specified image onto a surface
+SDL_Surface* loadSurface(std::string path);
+
+// The main window we'll be rendering to. Parent to all surfaces.
 SDL_Window* gWindow = NULL;
 
+// The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
 
-SDL_Surface* gHelloWorld = NULL;
+// Maps key presses to an image.
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
 
-bool quit = false;
+// Current displayed image
+SDL_Surface* gCurrentSurface = NULL;
 
-SDL_Event e;
+
+SDL_Surface* loadSurface(std::string path) {
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+    if (loadedSurface == NULL) {
+        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    }
+    return loadedSurface;
+}
+
+// Loads all the media that we're going to be using.
+// Is it more efficient to do it this way than on the fly?
+// This seems like it can kill performance
+// if there are thousands of images being used.
+bool loadMedia() {
+    bool success = true;
+
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("press.bmp");
+    if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL) {
+        printf("Failed to load default image: %s", SDL_GetError());
+        success = false;
+    }
+
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("up.bmp");
+    if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL) {
+        printf("Failed to load up image: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("down.bmp");
+    if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL) {
+        printf("Failed to load down image: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("left.bmp");
+    if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL) {
+        printf("Failed to load left image: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("right.bmp");
+    if (gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL) {
+        printf("Failed to load right image: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
 
 bool init() {
     bool success = true;
@@ -34,21 +108,14 @@ bool init() {
 }
 
 
-bool loadMedia() {
-    bool success = true;
-
-    gHelloWorld = SDL_LoadBMP("marbles.bmp");
-    if (gHelloWorld == NULL) {
-        printf("Could not load media: %s\n", SDL_GetError());
-        success = false;
-    }
-
-    return success;
-}
-
 void close() {
-    SDL_FreeSurface(gHelloWorld);
-    gHelloWorld = NULL;
+    SDL_FreeSurface(gCurrentSurface);
+    gCurrentSurface = NULL;
+
+    // Might not need to do this since it's the main window surface.
+    // It probably gets cleaned up in SDL_DestroyWindow
+    //SDL_FreeSurface(gScreenSurface);
+    //gScreenSurface = NULL;
 
     SDL_DestroyWindow(gWindow);
     gWindow = NULL;
@@ -57,23 +124,42 @@ void close() {
 }
 
 int main(int argc, char* args[]) {
-    if (init() >= 0) {
-        if (loadMedia() >= 0) {
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-            SDL_UpdateWindowSurface(gWindow);
+    bool quit = false;
 
-            while (!quit) {
-                while (SDL_PollEvent(&e) != 0) {
+    SDL_Event e;
+
+
+    if (init()) {
+        if (loadMedia()) {
+            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
+            while(!quit) {
+                while(SDL_PollEvent(&e) != 0) {
+
                     if (e.type == SDL_QUIT) {
                         quit = true;
+                    } else if (e.type == SDL_KEYDOWN){
+                        switch (e.key.keysym.sym) {
+                            case SDLK_UP:
+                                gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+                                break;
+                            case SDLK_DOWN:
+                                gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+                                break;
+                            case SDLK_LEFT:
+                                gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+                                break;
+                            case SDLK_RIGHT:
+                                gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+                                break;
+                        }
                     }
                 }
+
+                SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
+                SDL_UpdateWindowSurface(gWindow);
             }
-        } else {
-            printf("Could not load media");
         }
-    } else {
-        printf("Could not init");
     }
 
     close();
